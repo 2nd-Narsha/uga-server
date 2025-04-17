f6eb5af581487a59abea4e724f70e775e193e584
Ljh081021 <loveljh081021@dgsw.hs.kr>
2025-04-17 20:07:14 +0900

    first commit

---
+++ src/main/java/com/example/uga/UgaApplication.java
@@ -0,0 +1,13 @@
+package com.example.uga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UgaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UgaApplication.class, args);
+    }
+
+}
