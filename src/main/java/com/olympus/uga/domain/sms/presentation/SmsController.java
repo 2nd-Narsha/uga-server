d168174b490970449e554dcf659d205d36cbd3e3
tlsalsco41122 <tlsalsco41122@gmail.com>
2025-05-08 20:10:34 +0900

    feat :: SmsController

--- src/main/java/com/olympus/uga/domain/sms/presentation/SmsController.java
+++ src/main/java/com/olympus/uga/domain/sms/presentation/SmsController.java
@@ -2,29 +2,28 @@ package com.olympus.uga.domain.sms.presentation;
 
 import com.olympus.uga.domain.sms.presentation.dto.request.SmsSendingReq;
 import com.olympus.uga.domain.sms.service.SmsService;
+import com.olympus.uga.global.common.Response;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/sms")
 @RequiredArgsConstructor
 public class SmsController {
-
     private final SmsService smsService;
 
     @PostMapping("/send")
-    public void sendMessage(@RequestBody SmsSendingReq req){
-
+    public void sendMessage(@RequestBody SmsSendingReq req) {
+        Response.of(smsService.sendMessage(req));
     }
 
-    //    @PostMapping("/send")
-    //    fun sendEmail(@RequestBody request: EmailSendingRequest) =
-    //        BaseResponse.of(emailService.sendEmail(request))
-    //
-    //    @GetMapping("/verify")
-    //    fun verifyEmail(@RequestParam email: String, @RequestParam code: String) =
-    //        BaseResponse.of(emailService.verifyEmail(email, code))
+    @GetMapping("/verify")
+    public void verifyMessage(@RequestParam String phoneNum, @RequestParam String code){
+        Response.of(smsService.verifyMessage(phoneNum, code));
+    }
 }
