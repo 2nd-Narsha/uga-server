47b49b6b2b5957a4f1bf02ca00a4752e8a9e1822
tlsalsco41122 <tlsalsco41122@gmail.com>
2025-05-08 20:50:06 +0900

    feat :: AuthErrorCode

---
+++ src/main/java/com/olympus/uga/domain/auth/error/AuthErrorCode.java
@@ -0,0 +1,18 @@
+package com.olympus.uga.domain.auth.error;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import com.olympus.uga.global.exception.error.CustomErrorCode;
+
+@Getter
+@RequiredArgsConstructor
+public enum AuthErrorCode implements CustomErrorCode {
+    PHONE_NUM_ALREADY(HttpStatus.CONFLICT, "이미 존재하는 전화번호입니다."),
+    USER_ALREADY(HttpStatus.CONFLICT, "이미 존재하는 유저입니다."),
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "유저를 찾을 수 없습니다."),
+    PASSWORD_WRONG(HttpStatus.BAD_REQUEST, "비밀번호가 맞지 않습니다.");
+
+    private final HttpStatus status;
+    private final String message;
+}
