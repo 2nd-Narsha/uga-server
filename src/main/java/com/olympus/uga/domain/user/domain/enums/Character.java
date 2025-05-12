2f8c94286d714aa1c883b01ef4a175d090115da1
tlsalsco41122 <tlsalsco41122@gmail.com>
2025-05-12 10:46:39 +0900

    .

--- src/main/java/com/olympus/uga/domain/user/domain/enums/Character.java
+++ src/main/java/com/olympus/uga/domain/user/domain/enums/Character.java
@@ -1,6 +1,6 @@
 package com.olympus.uga.domain.user.domain.enums;
 
-public enum Character {
+public enum Character { // 캐릭터 이름
     SOBORO, CHUNBOGI, BERINGKEON, BERRY, CHARLES, BONGGU,
     NICHOLAS, MILK, HODU, NAELLEUM, OGONGI, PENGDUGI, ACHIMI
 }
