630f86db30e3b388a8336778e58f76470fa91f17
tlsalsco41122 <tlsalsco41122@gmail.com>
2025-05-13 11:54:36 +0900

    feat :: kakao setting

--- src/main/java/com/olympus/uga/global/config/SpringConfig.java
+++ src/main/java/com/olympus/uga/global/config/SpringConfig.java
@@ -1,9 +1,11 @@
 package com.olympus.uga.global.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class SpringConfig {
@@ -15,5 +17,10 @@ public class SpringConfig {
     @Bean
     public ObjectMapper objectMapper(){
         return new ObjectMapper();
+    }
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
+        return restTemplateBuilder.build();
     }
 }
