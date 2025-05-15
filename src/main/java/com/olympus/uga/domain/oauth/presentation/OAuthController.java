13fc7d0fe8446fce3ff52431b89f590647d318d1
tlsalsco41122 <tlsalsco41122@gmail.com>
2025-05-15 20:56:46 +0900

    fix :: oauth how to get access token

--- src/main/java/com/olympus/uga/domain/oauth/presentation/OAuthController.java
+++ src/main/java/com/olympus/uga/domain/oauth/presentation/OAuthController.java
@@ -1,13 +1,12 @@
 package com.olympus.uga.domain.oauth.presentation;
 
 import com.olympus.uga.domain.auth.presentation.dto.response.SignInRes;
-import com.olympus.uga.domain.oauth.presentation.dto.request.OAuthLoginReq;
 import com.olympus.uga.domain.oauth.usecase.OAuthUseCase;
 import com.olympus.uga.global.common.ResponseData;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,13 +16,13 @@ public class OAuthController {
     private final OAuthUseCase oAuthUseCase;
 
     @PostMapping("/kakao/login")
-    public ResponseData<SignInRes> kakaoLogin(@RequestBody OAuthLoginReq req) {
-        return oAuthUseCase.loginWithKakao(req);
+    public ResponseData<SignInRes> kakaoLogin(@RequestParam String code) {
+        return oAuthUseCase.loginWithKakaoCode(code);
     }
 
     @PostMapping("/google/login")
-    public ResponseData<SignInRes> googleLogin(@RequestBody OAuthLoginReq req) {
-        return oAuthUseCase.loginWithGoogle(req);
+    public ResponseData<SignInRes> googleLogin(@RequestParam String code) {
+        return oAuthUseCase.loginWithGoogleCode(code);
 
     }
 }
