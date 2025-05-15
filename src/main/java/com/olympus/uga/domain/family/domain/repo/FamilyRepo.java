e13306e9ef6510a84bf619c111e0cdf4c9f6c86f
Ljh081021 <loveljh081021@dgsw.hs.kr>
2025-05-15 19:26:23 +0900

    fix :: family member requestParam

--- src/main/java/com/olympus/uga/domain/family/domain/repo/FamilyRepo.java
+++ src/main/java/com/olympus/uga/domain/family/domain/repo/FamilyRepo.java
@@ -7,6 +7,6 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface FamilyRepo extends JpaRepository<Family, Long> {
+public interface FamilyRepo extends JpaRepository<Family, String> {
     Optional<Family> findByFamilyCode(String familyCode);
 }
