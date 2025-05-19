96e1bb5dcf1e382bdd3ebc4330262c57a3989ab1
tlsalsco41122 <tlsalsco41122@gmail.com>
2025-05-19 09:59:55 +0900

    feat :: user setting dto

--- src/main/java/com/olympus/uga/domain/user/presentation/UserSettingController.java
+++ src/main/java/com/olympus/uga/domain/user/presentation/UserSettingController.java
@@ -1,12 +1,17 @@
 package com.olympus.uga.domain.user.presentation;
 
-import com.olympus.uga.domain.user.presentation.dto.UserSettingReq;
+import com.olympus.uga.domain.user.presentation.dto.CharacterReq;
+import com.olympus.uga.domain.user.presentation.dto.InterestReq;
+import com.olympus.uga.domain.user.presentation.dto.MbtiReq;
+import com.olympus.uga.domain.user.presentation.dto.UsernameBirthGenderReq;
 import com.olympus.uga.domain.user.service.UserSettingService;
 import com.olympus.uga.global.common.Response;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,8 +20,27 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserSettingController {
     private final UserSettingService userSettingService;
 
-    @PostMapping
-    public Response userSetting(@RequestBody UserSettingReq req) {
-        return userSettingService.userSetting(req);
+    @PostMapping("/1")
+    @Operation(summary = "이름, 생년월일, 성별", description = "성별: MALE, FEMALE")
+    public Response setUsernameBirthGender(@RequestBody UsernameBirthGenderReq req) {
+        return userSettingService.setUsernameBirth(req);
+    }
+
+    @PostMapping("/2")
+    @Operation(summary = "관심 주제")
+    public Response setInterest(@RequestBody InterestReq req) {
+        return userSettingService.setInterest(req);
+    }
+
+    @PostMapping("/3")
+    @Operation(summary = "캐릭터")
+    public Response setCharacter(@RequestParam CharacterReq req) {
+        return userSettingService.setCharacter(req);
+    }
+
+    @PostMapping("/4")
+    @Operation(summary = "mbti")
+    public Response setMbti(@RequestBody MbtiReq req) {
+        return userSettingService.setMbti(req);
     }
 }
---
+++ src/main/java/com/olympus/uga/domain/user/presentation/dto/CharacterReq.java
@@ -0,0 +1,6 @@
+package com.olympus.uga.domain.user.presentation.dto;
+
+import com.olympus.uga.domain.user.domain.enums.UserCharacter;
+
+public record CharacterReq(UserCharacter character) {
+}
