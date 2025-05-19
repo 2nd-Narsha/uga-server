c6637cf1c7182ab33e283fb11066586fab4adbbf
Ljh081021 <loveljh081021@dgsw.hs.kr>
2025-05-19 20:54:47 +0900

    feat :: image function

--- src/main/java/com/olympus/uga/domain/family/presentation/dto/request/FamilyCreateReq.java
+++ src/main/java/com/olympus/uga/domain/family/presentation/dto/request/FamilyCreateReq.java
@@ -1,6 +1,7 @@
 package com.olympus.uga.domain.family.presentation.dto.request;
 
 import lombok.Data;
+import org.springframework.web.multipart.MultipartFile;
 
 @Data
 public class FamilyCreateReq {
---
+++ src/main/java/com/olympus/uga/domain/image/error/ImageErrorCode.java
@@ -0,0 +1,20 @@
+package com.olympus.uga.domain.image.error;
+
+import com.olympus.uga.global.exception.error.CustomErrorCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum ImageErrorCode implements CustomErrorCode {
+
+    FILE_UPLOAD_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "파일 업로드에 실패했습니다."),
+    FILE_EMPTY(HttpStatus.BAD_REQUEST, "업로드한 파일이 비어 있습니다."),
+    FILE_DELETE_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "파일 삭제에 실패했습니다."),
+    FILE_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 이름의 이미지를 찾을 수 없습니다."),
+    FILE_CONVERT_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "파일 변환 중 문제가 발생했습니다.");
+
+    private final HttpStatus status;
+    private final String message;
+}
