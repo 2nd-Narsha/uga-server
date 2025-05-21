f424ddf6693a5fe44cef6dcf0e5f8b8ed4f3b8b7
Ljh081021 <loveljh081021@dgsw.hs.kr>
2025-05-21 19:54:37 +0900

    fix :: family image

---
+++ src/main/java/com/olympus/uga/domain/family/error/FamilyErrorCode.java
@@ -0,0 +1,16 @@
+package com.olympus.uga.domain.family.error;
+
+import com.olympus.uga.global.exception.error.CustomErrorCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum FamilyErrorCode implements CustomErrorCode {
+    FAMILY_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 코드의 가족을 찾을 수 없습니다."),
+    NOT_FAMILY_MEMBER(HttpStatus.BAD_REQUEST, "해당 가족의 멤버가 아닙니다.");
+
+    private final HttpStatus status;
+    private final String message;
+}
