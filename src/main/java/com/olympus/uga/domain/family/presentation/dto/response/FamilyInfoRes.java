5d4c354555e873a881cfe6934477fa7fbf8f260f
Ljh081021 <loveljh081021@dgsw.hs.kr>
2025-05-22 20:10:46 +0900

    feat :: change leader

--- src/main/java/com/olympus/uga/domain/family/presentation/dto/response/FamilyInfoRes.java
+++ src/main/java/com/olympus/uga/domain/family/presentation/dto/response/FamilyInfoRes.java
@@ -1,6 +1,9 @@
 package com.olympus.uga.domain.family.presentation.dto.response;
 
 import com.olympus.uga.domain.family.domain.Family;
+import com.olympus.uga.domain.uga.domain.Uga;
+import com.olympus.uga.domain.user.domain.User;
+import com.olympus.uga.domain.user.domain.repo.UserJpaRepo;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -10,20 +13,20 @@ public class FamilyInfoRes {
     private String familyCode;
     private String familyName;
     private int presentUgaId;
+    private int point;
     private String profileLink;
     private String representativePhoneNum;
-    private List<String> familyUgaList;
-    private List<String> memberList;
+    private List<User> memberList;
     private LocalDateTime createdAt;
 
-    public FamilyInfoRes(Family family) {
+    public FamilyInfoRes(Family family, List<User> memberList) {
         this.familyCode = family.getFamilyCode();
         this.familyName = family.getFamilyName();
         this.presentUgaId = family.getPresentUgaId();
+        this.point = family.getPoint();
         this.profileLink = family.getProfileLink();
         this.representativePhoneNum = family.getRepresentativePhoneNum();
-        this.familyUgaList = family.getFamilyUgaList();
-        this.memberList = family.getMemberList();
+        this.memberList = memberList;
         this.createdAt = family.getCreatedAt();
 
     }
--- src/main/java/com/olympus/uga/domain/uga/domain/Uga.java
+++
@@ -1,5 +0,0 @@
-package com.olympus.uga.domain.uga.domain;
-
-public class Uga {
-
-}
