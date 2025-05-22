f64bc4d9948ab2c9d12cf1423bf644ed1b707ab1
tlsalsco41122 <tlsalsco41122@gmail.com>
2025-05-22 20:24:12 +0900

    feat :: delete user

--- src/main/java/com/olympus/uga/domain/user/presentation/UserController.java
+++ src/main/java/com/olympus/uga/domain/user/presentation/UserController.java
@@ -2,8 +2,11 @@ package com.olympus.uga.domain.user.presentation;
 
 import com.olympus.uga.domain.user.presentation.dto.response.UserResponse;
 import com.olympus.uga.domain.user.service.UserService;
+import com.olympus.uga.global.common.Response;
 import com.olympus.uga.global.common.ResponseData;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,7 +18,14 @@ public class UserController {
     private final UserService userService;
 
     @GetMapping("/me")
+    @Operation(summary = "마이페이지 정보")
     public ResponseData<UserResponse> getMe() {
         return userService.getMe();
+    }
+
+    @DeleteMapping("/delete")
+    @Operation(summary = "회원탈퇴")
+    public Response deleteUser() {
+        return userService.deleteUser();
     }
 }
