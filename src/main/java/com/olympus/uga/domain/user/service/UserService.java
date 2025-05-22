24081e5519f20b02715fcb6de527b3ffd027e385
tlsalsco41122 <tlsalsco41122@gmail.com>
2025-05-22 20:24:12 +0900

    feat :: delete user

--- src/main/java/com/olympus/uga/domain/user/service/UserService.java
+++ src/main/java/com/olympus/uga/domain/user/service/UserService.java
@@ -1,7 +1,9 @@
 package com.olympus.uga.domain.user.service;
 
 import com.olympus.uga.domain.user.domain.User;
+import com.olympus.uga.domain.user.domain.repo.UserJpaRepo;
 import com.olympus.uga.domain.user.presentation.dto.response.UserResponse;
+import com.olympus.uga.global.common.Response;
 import com.olympus.uga.global.common.ResponseData;
 import com.olympus.uga.global.security.auth.UserSessionHolder;
 import lombok.RequiredArgsConstructor;
@@ -12,11 +14,20 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class UserService {
     private final UserSessionHolder userSessionHolder;
+    private final UserJpaRepo userJpaRepo;
 
     @Transactional
     public ResponseData<UserResponse> getMe() {
         User user = userSessionHolder.getUser();
 
         return ResponseData.ok("사용자 정보를 성공적으로 가져왔습니다.", UserResponse.getUsername(user));
+    }
+
+    public Response deleteUser() {
+        User user = userSessionHolder.getUser();
+
+        userJpaRepo.deleteById(user.getId());
+
+        return Response.ok("회원탈퇴에 성공하였습니다.");
     }
 }
