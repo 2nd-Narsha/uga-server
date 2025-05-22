0f6073a62d92a5baf265623e92129761afe378d8
tlsalsco41122 <tlsalsco41122@gmail.com>
2025-05-22 16:21:03 +0900

    remove :: CharacterReq

--- src/main/java/com/olympus/uga/domain/user/presentation/dto/request/CharacterReq.java
+++
@@ -1,6 +0,0 @@
-package com.olympus.uga.domain.user.presentation.dto.request;
-
-import com.olympus.uga.domain.user.domain.enums.UserCharacter;
-
-public record CharacterReq(UserCharacter character) {
-}
