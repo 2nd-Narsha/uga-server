c5d310d1b9bae35937d7e9a92f7ab06ac7704afe
Ljh081021 <loveljh081021@dgsw.hs.kr>
2025-05-26 16:57:01 +0900

    fix :: food shortage

--- src/main/java/com/olympus/uga/domain/uga/service/UgaService.java
+++ src/main/java/com/olympus/uga/domain/uga/service/UgaService.java
@@ -12,7 +12,9 @@ import com.olympus.uga.domain.uga.presentation.dto.req.UgaCreateReq;
 import com.olympus.uga.domain.uga.presentation.dto.req.UgaFeedReq;
 import com.olympus.uga.domain.uga.presentation.dto.res.UgaInfoRes;
 import com.olympus.uga.domain.uga.presentation.dto.res.UgaListRes;
+import com.olympus.uga.domain.user.domain.User;
 import com.olympus.uga.domain.user.domain.repo.UserJpaRepo;
+import com.olympus.uga.domain.user.error.UserErrorCode;
 import com.olympus.uga.global.common.Response;
 import com.olympus.uga.global.exception.CustomException;
 import jakarta.transaction.Transactional;
@@ -48,9 +50,19 @@ public class UgaService {
         return Response.created("당신의 우가 " + ugaCreateReq.getUgaName() + "가 생성되었습니다.");
     }
 
-    //일단 음식이 존재 하는 지 여부 확인
     //가족 기여도
     public Response ugaFeed(UgaFeedReq ugaFeedReq) {
+
+        User user = userJpaRepo.findById(SecurityContextHolder.getContext().getAuthentication().getName())
+                .orElseThrow(() -> new CustomException(UserErrorCode.USER_NOT_FOUND));
+
+        FoodType foodType = ugaFeedReq.getFoodType();
+
+        if (!user.getFoods().contains(foodType)) {
+            throw new CustomException(UgaErrorCode.FOOD_SHORTAGE);
+        }
+
+        user.getFoods().remove(foodType);
 
         Uga uga = ugaRepo.findById(ugaFeedReq.getUgaId())
                 .orElseThrow(() -> new CustomException(UgaErrorCode.UGA_NOT_FOUND));
@@ -61,14 +73,13 @@ public class UgaService {
                 FoodType.BANANA_KICK, 7
         );
 
-        int minusDays = foodEffect.getOrDefault(ugaFeedReq.getFoodType(), 0);
+        int minusDays = foodEffect.getOrDefault(foodType, 0);
 
         if (minusDays == 0) {
             throw new CustomException(UgaErrorCode.INVALID_FOOD_TYPE);
         }
 
         uga.setCompleteGrowthTime(uga.getCompleteGrowthTime().minusDays(minusDays));
-
         resetUga(uga);
 
         return Response.ok("우가에게 먹이를 주었습니다.");
