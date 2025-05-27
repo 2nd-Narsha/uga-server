56b454f9d86683b7e8eb5525ca788009602be652
Ljh081021 <loveljh081021@dgsw.hs.kr>
2025-05-28 08:45:31 +0900

    fix :: change findByImageName name in ImageRepo

--- src/main/java/com/olympus/uga/global/image/domain/repo/ImageRepo.java
+++ src/main/java/com/olympus/uga/global/image/domain/repo/ImageRepo.java
@@ -8,5 +8,5 @@ import java.util.Optional;
 
 @Repository
 public interface ImageRepo extends JpaRepository<ImageDetails, Long> {
-    Optional<ImageDetails> findByFileName(String fileName);
+    Optional<ImageDetails> findByImageName(String fileName);
 }
