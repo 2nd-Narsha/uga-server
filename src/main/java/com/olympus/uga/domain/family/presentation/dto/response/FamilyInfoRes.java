4448f4e92b72376837a04bcbcdcee801b59fae00
tlsalsco41122 <tlsalsco41122@gmail.com>
2025-05-29 22:02:14 +0900

    fix :: family

--- src/main/java/com/olympus/uga/domain/family/presentation/dto/response/FamilyInfoRes.java
+++ src/main/java/com/olympus/uga/domain/family/presentation/dto/response/FamilyInfoRes.java
@@ -1,29 +1,20 @@
 package com.olympus.uga.domain.family.presentation.dto.response;
 
-import com.olympus.uga.domain.family.domain.Family;
-import com.olympus.uga.domain.user.domain.User;
-
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 import java.util.List;
 
-public class FamilyInfoRes {
-
-    private String familyCode;
-    private String familyName;
-    private Long presentUgaId;
-    private String profileLink;
-    private Long representativeId;
-    private List<User> memberList;
-    private LocalDateTime createdAt;
-
-    public FamilyInfoRes(Family family, List<User> memberList) {
-        this.familyCode = family.getFamilyCode();
-        this.familyName = family.getFamilyName();
-        this.presentUgaId = family.getPresentUgaId();
-        this.profileLink = family.getProfileLink();
-        this.representativeId = family.getRepresentativeId();
-        this.memberList = memberList;
-        this.createdAt = family.getCreatedAt();
-
-    }
+public record FamilyInfoRes(
+        String familyName,
+        String familyCode,
+        String profileImage,
+        Long leaderId,
+        List<FamilyMemberInfo> members
+) {
+    public record FamilyMemberInfo(
+            Long id,
+            String username,
+            String birth,
+            String interests,
+            boolean isLeader
+    ) {}
 }
