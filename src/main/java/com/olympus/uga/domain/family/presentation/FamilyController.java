3299821a8f8ccd081765ae0fbca8470f41f9586b
tlsalsco41122 <tlsalsco41122@gmail.com>
2025-05-29 22:02:14 +0900

    fix :: family

--- src/main/java/com/olympus/uga/domain/family/presentation/FamilyController.java
+++ src/main/java/com/olympus/uga/domain/family/presentation/FamilyController.java
@@ -6,6 +6,7 @@ import com.olympus.uga.domain.family.presentation.dto.response.FamilyInfoRes;
 import com.olympus.uga.domain.family.service.FamilyService;
 import com.olympus.uga.global.common.Response;
 import com.olympus.uga.global.common.ResponseData;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -26,17 +27,19 @@ public class FamilyController {
         return familyService.joinFamily(familyCode);
     }
 
-    @GetMapping("/members")
-    public FamilyInfoRes getMembers() {
+    @GetMapping
+    @Operation(summary = "가족 정보 불러오기")
+    public ResponseData<FamilyInfoRes> getFamily() {
         return familyService.getFamily();
     }
 
     @PostMapping("/leave")
-    public Response leaveFamily() {
-        return familyService.leaveFamily();
+    public Response leaveFamily(String familyCode) {
+        return familyService.leaveFamily(familyCode);
     }
 
     @PostMapping("/change-leader")
+    @Operation(summary = "가족 리더 넘기기")
     public Response changeLeader(@RequestBody LeaderChangeReq req) {
         return familyService.changeLeader(req);
     }
--- src/main/java/com/olympus/uga/domain/family/service/FamilyService.java
+++ src/main/java/com/olympus/uga/domain/family/service/FamilyService.java
@@ -9,6 +9,7 @@ import com.olympus.uga.domain.family.presentation.dto.response.FamilyInfoRes;
 import com.olympus.uga.domain.family.util.CodeGenerator;
 import com.olympus.uga.domain.user.domain.User;
 import com.olympus.uga.domain.user.domain.repo.UserJpaRepo;
+import com.olympus.uga.domain.user.error.UserErrorCode;
 import com.olympus.uga.global.common.ResponseData;
 import com.olympus.uga.global.image.service.ImageService;
 import com.olympus.uga.global.common.Response;
@@ -16,10 +17,10 @@ import com.olympus.uga.global.exception.CustomException;
 import com.olympus.uga.global.security.auth.UserSessionHolder;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
@@ -37,20 +38,25 @@ public class FamilyService {
         User user = userSessionHolder.getUser();
         String code = codeGenerator.generateCode();
 
-        while (familyJpaRepo.findByFamilyCode(code).isPresent()) {
+        while (familyJpaRepo.findById(code).isPresent()) {
             code = codeGenerator.generateCode();
         }
 
         familyJpaRepo.save(FamilyCreateReq.fromFamilyCreateReq(code, req, user.getId(), imageService.uploadImage(familyProfile).getImageUrl()));
+
         return ResponseData.created("가족 생성에 성공했습니다.", code);
     }
 
     //가족 가입
     @Transactional
     public Response joinFamily(String familyCode) {
-        Family family = familyJpaRepo.findByFamilyCode(familyCode)
+        Family family = familyJpaRepo.findById(familyCode)
                 .orElseThrow(() -> new CustomException(FamilyErrorCode.FAMILY_NOT_FOUND));
         User user = userSessionHolder.getUser();
+
+        if (family.getMemberList().contains(user.getId())) {
+            throw new CustomException(FamilyErrorCode.ALREADY_MEMBER);
+        }
 
         family.getMemberList().add(user.getId());
 
@@ -58,42 +64,54 @@ public class FamilyService {
     }
 
     //가족 조회
-    public FamilyInfoRes getFamily() {
-        Family family = familyJpaRepo.findAll()
-                .stream()
-                .filter(f -> f.getMemberList().contains(SecurityContextHolder.getContext().getAuthentication().getName()))
-                .findFirst()
+    public ResponseData<FamilyInfoRes> getFamily() {
+        User user = userSessionHolder.getUser();
+
+        Family family = familyJpaRepo.findByMemberListContaining(user.getId())
                 .orElseThrow(() -> new CustomException(FamilyErrorCode.FAMILY_NOT_FOUND));
 
-        return new FamilyInfoRes(
-                family,
-                family.getMemberList().stream()
-                        .map(f -> userJpaRepo.findById(f)
-                                .orElseThrow(() -> new CustomException(FamilyErrorCode.NOT_FAMILY_MEMBER)))
-                        .collect(Collectors.toList())
-                );
+        // 멤버 정보 조회 및 변환
+        List<FamilyInfoRes.FamilyMemberInfo> memberInfos = family.getMemberList().stream()
+                .map(memberId -> {
+                    User memberUser = userJpaRepo.findById(memberId)
+                            .orElseThrow(() -> new CustomException(UserErrorCode.USER_NOT_FOUND));
+
+                    return new FamilyInfoRes.FamilyMemberInfo(
+                            memberUser.getId(),
+                            memberUser.getUsername(),
+                            memberUser.getBirth(),
+                            memberUser.getInterests(),
+                            family.getLeaderId().equals(memberUser.getId()) // 리더 여부
+                    );
+                })
+                .collect(Collectors.toList());
+
+        FamilyInfoRes response = new FamilyInfoRes(
+                family.getFamilyName(),
+                family.getFamilyCode(),
+                family.getProfileImage(),
+                family.getLeaderId(),
+                memberInfos
+        );
+
+        return ResponseData.ok("가족 정보 조회에 성공했습니다.", response);
     }
 
     //가족 떠나기
     @Transactional
-    public Response leaveFamily() {
-        Family family = familyJpaRepo.findAll()
-                .stream()
-                .filter(f -> f.getMemberList().contains(SecurityContextHolder.getContext().getAuthentication().getName()))
-                .findFirst()
+    public Response leaveFamily(String code) {
+        Family family = familyJpaRepo.findById(code)
                 .orElseThrow(() -> new CustomException(FamilyErrorCode.FAMILY_NOT_FOUND));
+        User user = userSessionHolder.getUser();
 
-        if (family.getMemberList().contains(SecurityContextHolder.getContext().getAuthentication().getName())) {
-            family.getMemberList().remove(SecurityContextHolder.getContext().getAuthentication().getName());
-        } else {
-            throw new CustomException(FamilyErrorCode.NOT_FAMILY_MEMBER);
-        }
+        family.getMemberList().remove(user.getId());
+
         return Response.ok("가족 " + family.getFamilyName() + "을/를 떠나셨습니다.");
     }
 
     //리더 넘기기
     public Response changeLeader(LeaderChangeReq req) {
-        Family family = familyJpaRepo.findByFamilyCode(req.familyCode())
+        Family family = familyJpaRepo.findById(req.familyCode())
                 .orElseThrow(() -> new CustomException(FamilyErrorCode.FAMILY_NOT_FOUND));
 
         family.updateLeader(req.id());
