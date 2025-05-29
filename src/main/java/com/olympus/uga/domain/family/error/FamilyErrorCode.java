a0725a97793232d9a1f18d9041dd3e0c90942b3a
tlsalsco41122 <tlsalsco41122@gmail.com>
2025-05-29 17:20:41 +0900

    fix :: FamilyErrorCode

--- src/main/java/com/olympus/uga/domain/family/error/FamilyErrorCode.java
+++ src/main/java/com/olympus/uga/domain/family/error/FamilyErrorCode.java
@@ -11,7 +11,6 @@ public enum FamilyErrorCode implements CustomErrorCode {
     FAMILY_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 코드의 가족을 찾을 수 없습니다."),
     NOT_FAMILY_MEMBER(HttpStatus.BAD_REQUEST, "해당 가족의 멤버가 아닙니다."),
     NOT_FAMILY_UGA(HttpStatus.BAD_REQUEST, "해당 가족의 우가가 아닙니다.");
-    NOT_FAMILY_MEMBER(HttpStatus.BAD_REQUEST, "해당 가족의 멤버가 아닙니다.");
 
     private final HttpStatus status;
     private final String message;
