cbff91f60f176918237c61c9c806682c5f1fea43
tlsalsco41122 <tlsalsco41122@gmail.com>
2025-06-03 01:31:51 +0900

    fix :: uga controller, dto

--- src/main/java/com/olympus/uga/domain/uga/presentation/UgaController.java
+++ src/main/java/com/olympus/uga/domain/uga/presentation/UgaController.java
@@ -6,6 +6,7 @@ import com.olympus.uga.domain.uga.presentation.dto.response.UgaInfoRes;
 import com.olympus.uga.domain.uga.presentation.dto.response.UgaListRes;
 import com.olympus.uga.domain.uga.service.UgaService;
 import com.olympus.uga.global.common.Response;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,22 +18,26 @@ import java.util.List;
 public class UgaController {
     private final UgaService ugaService;
 
+    @GetMapping
+    @Operation(summary = "우가 불러오기")
+    public UgaInfoRes getUga(@RequestParam Long ugaId) {
+        return ugaService.getUga(ugaId);
+    }
+
     @PostMapping("/create")
+    @Operation(summary = "우가 생성")
     public Response createFamily(@RequestBody UgaCreateReq req) {
         return ugaService.createUga(req);
     }
 
     @PostMapping("/feed")
+    @Operation(summary = "우가 먹이주기")
     public Response feedUga(@RequestBody UgaFeedReq req) {
         return ugaService.ugaFeed(req);
     }
 
-    @GetMapping("/get")
-    public UgaInfoRes getUga(@RequestParam Long ugaId) {
-        return ugaService.getUga(ugaId);
-    }
-
-    @GetMapping("/ugalist")
+    @GetMapping("/ugaList")
+    @Operation(summary = "우가 사전")
     public List<UgaListRes> getUgaList() {
         return ugaService.getUgaList();
     }
