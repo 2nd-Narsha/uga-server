9ddd8145b52d7632610a50ed99fc983d5bfe4e7a
tlsalsco41122 <tlsalsco41122@gmail.com>
2025-06-03 01:31:51 +0900

    fix :: uga controller, dto

--- src/main/java/com/olympus/uga/domain/uga/presentation/dto/response/UgaListRes.java
+++ src/main/java/com/olympus/uga/domain/uga/presentation/dto/response/UgaListRes.java
@@ -1,16 +1,6 @@
 package com.olympus.uga.domain.uga.presentation.dto.response;
 
-import com.olympus.uga.domain.uga.domain.Uga;
-import com.olympus.uga.domain.uga.domain.enums.UgaGrowth;
+import java.time.LocalDateTime;
 
-public class UgaListRes {
-    private Long ugaId;
-    private String name;
-    private UgaGrowth growth;
-
-    public UgaListRes(Uga uga) {
-        this.ugaId = uga.getUgaId();
-        this.name = uga.getName();
-        this.growth = uga.getGrowth();
-    }
+public record UgaListRes(String ugaName, LocalDateTime completeGrowthTime) {
 }
