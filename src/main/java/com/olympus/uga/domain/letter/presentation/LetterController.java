904bfd2e894beb99d54481f9ba6ed5dea0f8d336
tlsalsco41122 <tlsalsco41122@gmail.com>
2025-06-03 18:36:44 +0900

    feat :: letter write

--- src/main/java/com/olympus/uga/domain/letter/presentation/LetterController.java
+++ src/main/java/com/olympus/uga/domain/letter/presentation/LetterController.java
@@ -1,8 +1,11 @@
 package com.olympus.uga.domain.letter.presentation;
 
+import com.olympus.uga.domain.letter.presentation.dto.request.LetterReq;
 import com.olympus.uga.domain.letter.service.LetterService;
+import com.olympus.uga.global.common.Response;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +14,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/letter")
 public class LetterController {
     private final LetterService letterService;
+
+    @PostMapping("/write")
+    public Response writeLetter(@RequestBody LetterReq req) {
+        return letterService.writeLetter(req);
+    }
 }
--- src/main/java/com/olympus/uga/domain/letter/service/LetterService.java
+++ src/main/java/com/olympus/uga/domain/letter/service/LetterService.java
@@ -1,9 +1,24 @@
 package com.olympus.uga.domain.letter.service;
 
+import com.olympus.uga.domain.letter.domain.repo.LetterJpaRepo;
+import com.olympus.uga.domain.letter.presentation.dto.request.LetterReq;
+import com.olympus.uga.domain.user.domain.User;
+import com.olympus.uga.global.common.Response;
+import com.olympus.uga.global.security.auth.UserSessionHolder;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class LetterService {
+    private final LetterJpaRepo letterJpaRepo;
+    private final UserSessionHolder userSessionHolder;
+
+    public Response writeLetter(LetterReq req) {
+        User user = userSessionHolder.getUser();
+
+        letterJpaRepo.save(LetterReq.fromLetterReq(user, req));
+
+        return Response.created(req.receiverId() + "에게 편지를 보냈습니다.");
+    }
 }
