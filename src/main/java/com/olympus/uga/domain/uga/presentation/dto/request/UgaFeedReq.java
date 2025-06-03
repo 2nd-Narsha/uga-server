acc4da82e1bfbea7ef5cb2eee70aab3080aa038a
tlsalsco41122 <tlsalsco41122@gmail.com>
2025-06-03 20:46:44 +0900

    fix

--- src/main/java/com/olympus/uga/domain/uga/presentation/dto/request/UgaFeedReq.java
+++
@@ -1,6 +0,0 @@
-package com.olympus.uga.domain.uga.presentation.dto.request;
-
-import com.olympus.uga.domain.uga.domain.enums.FoodType;
-
-public record UgaFeedReq(Long ugaId, FoodType foodType) {
-}
