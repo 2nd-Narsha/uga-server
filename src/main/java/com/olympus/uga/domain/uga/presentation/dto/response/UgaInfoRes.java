f2e8aa95ba7b82213e32155960e9e1edb6970746
tlsalsco41122 <tlsalsco41122@gmail.com>
2025-06-03 20:46:44 +0900

    fix

--- src/main/java/com/olympus/uga/domain/uga/presentation/dto/response/UgaInfoRes.java
+++
@@ -1,8 +0,0 @@
-package com.olympus.uga.domain.uga.presentation.dto.response;
-
-import com.olympus.uga.domain.uga.domain.enums.UgaGrowth;
-
-import java.time.LocalDateTime;
-
-public record UgaInfoRes(String UgaName, UgaGrowth growth, LocalDateTime createdAt, LocalDateTime completeGrowthTime) {
-}
