36bf67d4be083aa5f408296c50044ab5ab251da6
tlsalsco41122 <tlsalsco41122@gmail.com>
2025-06-12 19:35:05 +0900

    fix :: LocalDate

--- src/main/java/com/olympus/uga/global/config/SpringConfig.java
+++ src/main/java/com/olympus/uga/global/config/SpringConfig.java
@@ -1,6 +1,8 @@
 package com.olympus.uga.global.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,7 +17,10 @@ public class SpringConfig {
     }
 
     @Bean
-    public ObjectMapper objectMapper(){
-        return new ObjectMapper();
+    public ObjectMapper objectMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        return mapper;
     }
 }
