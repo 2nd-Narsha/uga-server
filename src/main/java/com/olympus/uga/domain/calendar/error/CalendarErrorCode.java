174ba933d812621fe7687a4377a34503fe3c9cdc
tlsalsco41122 <tlsalsco41122@gmail.com>
2025-06-17 09:00:38 +0900

    fix :: error comment

--- src/main/java/com/olympus/uga/domain/calendar/error/CalendarErrorCode.java
+++ src/main/java/com/olympus/uga/domain/calendar/error/CalendarErrorCode.java
@@ -8,7 +8,8 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum CalendarErrorCode implements CustomErrorCode {
-    DDAY_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 디데이를 찾을 수 없습니다.");
+    DDAY_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 디데이를 찾을 수 없습니다."),
+    SCHEDULE_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 일정을 찾을 수 없습니다.");
 
     private final HttpStatus status;
     private final String message;
--- src/main/java/com/olympus/uga/domain/letter/error/LetterErrorCode.java
+++ src/main/java/com/olympus/uga/domain/letter/error/LetterErrorCode.java
@@ -8,7 +8,7 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum LetterErrorCode implements CustomErrorCode {
-    LETTER_NOT_FOUND(HttpStatus.NOT_FOUND, "편지를 찾을 수 없습니다.");
+    LETTER_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 편지를 찾을 수 없습니다.");
 
     private final HttpStatus status;
     private final String message;
