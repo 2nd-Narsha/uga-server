8fde38782209bbf0f955558d64e4afa69b51a1c8
tlsalsco41122 <tlsalsco41122@gmail.com>
2025-06-17 09:00:38 +0900

    fix :: error comment

--- src/main/java/com/olympus/uga/domain/auth/error/AuthErrorCode.java
+++ src/main/java/com/olympus/uga/domain/auth/error/AuthErrorCode.java
@@ -9,7 +9,7 @@ import com.olympus.uga.global.exception.error.CustomErrorCode;
 @RequiredArgsConstructor
 public enum AuthErrorCode implements CustomErrorCode {
     PHONE_NUM_ALREADY(HttpStatus.CONFLICT, "이미 존재하는 전화번호입니다."),
-    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "유저를 찾을 수 없습니다."),
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 유저를 찾을 수 없습니다."),
     WRONG_PASSWORD(HttpStatus.BAD_REQUEST, "비밀번호가 맞지 않습니다.");
 
     private final HttpStatus status;
