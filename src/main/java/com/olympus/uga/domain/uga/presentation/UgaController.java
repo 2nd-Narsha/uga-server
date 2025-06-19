abf045190303bd71c39f6d1737431e0fcea3847e
tlsalsco41122 <tlsalsco41122@gmail.com>
2025-06-19 19:55:39 +0900

    feat :: uga dto

--- src/main/java/com/olympus/uga/domain/uga/presentation/UgaController.java
+++ src/main/java/com/olympus/uga/domain/uga/presentation/UgaController.java
@@ -3,6 +3,7 @@ package com.olympus.uga.domain.uga.presentation;
 import com.olympus.uga.domain.uga.presentation.dto.request.UgaCreateReq;
 import com.olympus.uga.domain.uga.service.UgaService;
 import com.olympus.uga.global.common.Response;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,6 +17,7 @@ public class UgaController {
     private final UgaService ugaService;
 
     @PostMapping("/create")
+    @Operation(summary = "우가 생성")
     public Response createUga(@RequestBody UgaCreateReq req){
         return ugaService.createUga(req);
     }
