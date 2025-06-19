aceecd156d705b26b92d43314587dcc04bedc3c9
tlsalsco41122 <tlsalsco41122@gmail.com>
2025-06-19 19:55:39 +0900

    feat :: uga dto

--- src/main/java/com/olympus/uga/domain/uga/presentation/dto/response/CurrentUgaRes.java
+++ src/main/java/com/olympus/uga/domain/uga/presentation/dto/response/CurrentUgaRes.java
@@ -2,5 +2,5 @@ package com.olympus.uga.domain.uga.presentation.dto.response;
 
 import com.olympus.uga.domain.uga.domain.enums.UgaGrowth;
 
-public record UgaRes(String ugaName, UgaGrowth growth) {
+public record CurrentUgaRes(String ugaName, UgaGrowth growth) {
 }
