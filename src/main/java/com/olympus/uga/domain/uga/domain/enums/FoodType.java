5832d7225bbbae28e027efc11e76d1adbc3615a2
tlsalsco41122 <tlsalsco41122@gmail.com>
2025-06-23 22:02:29 +0900

    comment :: uga

--- src/main/java/com/olympus/uga/domain/uga/domain/enums/FoodType.java
+++ src/main/java/com/olympus/uga/domain/uga/domain/enums/FoodType.java
@@ -1,5 +1,7 @@
 package com.olympus.uga.domain.uga.domain.enums;
 
 public enum FoodType {
-    BANANA_CHIP, BANANA, BANANA_KICK
+    BANANA_CHIP, // 20P, 하루
+    BANANA, // 55P, 3일
+    BANANA_KICK // 120P, 7일
 }
