208db68053671939df565ff9141976883743ef2b
tlsalsco41122 <tlsalsco41122@gmail.com>
2025-06-23 22:16:12 +0900

    comment

--- src/main/java/com/olympus/uga/domain/uga/domain/enums/UgaGrowth.java
+++ src/main/java/com/olympus/uga/domain/uga/domain/enums/UgaGrowth.java
@@ -1,10 +1,10 @@
 package com.olympus.uga.domain.uga.domain.enums;
 
 public enum UgaGrowth {
-    BABY, // 처음 생성
-    CHILD, // 92일 이상
-    TEENAGER, // 183일 이상
-    ADULT, // 274일 이상
+    BABY, // 처음 생성 ~
+    CHILD, // 92일 이상 ~
+    TEENAGER, // 183일 이상 ~
+    ADULT, // 274일 이상 ~
     ALL_GROWTH, // 365일 / 독립 안 시킴
     INDEPENDENCE // 독립시킨 후
 }
