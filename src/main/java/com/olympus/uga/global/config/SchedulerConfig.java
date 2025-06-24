380eb060be5eace4624a32c8b33a98551ff1201e
tlsalsco41122 <tlsalsco41122@gmail.com>
2025-06-24 09:37:57 +0900

    feat :: uga natural growth

---
+++ src/main/java/com/olympus/uga/global/config/SchedulerConfig.java
@@ -0,0 +1,10 @@
+package com.olympus.uga.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class SchedulerConfig {
+    // 스케줄링 기능 활성화
+}
