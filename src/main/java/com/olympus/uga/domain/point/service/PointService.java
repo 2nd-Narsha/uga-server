a36550d613d4a61754ca365561e126e7d30ce3aa
tlsalsco41122 <tlsalsco41122@gmail.com>
2025-06-24 09:37:57 +0900

    feat :: uga natural growth

--- src/main/java/com/olympus/uga/domain/point/service/PointService.java
+++ src/main/java/com/olympus/uga/domain/point/service/PointService.java
@@ -1,10 +1,15 @@
 package com.olympus.uga.domain.point.service;
 
+import com.olympus.uga.domain.family.domain.Family;
+import com.olympus.uga.domain.family.domain.repo.FamilyJpaRepo;
+import com.olympus.uga.domain.family.error.FamilyErrorCode;
 import com.olympus.uga.domain.point.domain.enums.ActivityType;
 import com.olympus.uga.domain.uga.domain.enums.FoodType;
+import com.olympus.uga.domain.uga.util.UgaFeedUtil;
 import com.olympus.uga.domain.user.domain.User;
 import com.olympus.uga.domain.user.domain.repo.UserJpaRepo;
 import com.olympus.uga.global.common.Response;
+import com.olympus.uga.global.exception.CustomException;
 import com.olympus.uga.global.security.auth.UserSessionHolder;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -17,6 +22,8 @@ import java.util.Map;
 public class PointService {
     private final UserJpaRepo userJpaRepo;
     private final UserSessionHolder userSessionHolder;
+    private final FamilyJpaRepo familyJpaRepo;
+
     private static final Map<ActivityType, Integer> ACTIVITY_POINTS = Map.of(
             ActivityType.SIGN_UP, 50,
             ActivityType.LETTER, 2,
@@ -53,8 +60,11 @@ public class PointService {
     public Response usePoint(FoodType food) {
         User user = userSessionHolder.getUser();
 
-        int foodPrice = FOOD_PRICES.getOrDefault(food, 0);
-        user.usePoint(foodPrice);
+        Family family = familyJpaRepo.findByMemberListContaining(user.getId())
+                .orElseThrow(() -> new CustomException(FamilyErrorCode.NOT_FAMILY_MEMBER));
+
+        int totalCost = UgaFeedUtil.calculateTotalCost(food, family.getMemberList().size());
+        user.usePoint(totalCost);
 
         userJpaRepo.save(user);
 
