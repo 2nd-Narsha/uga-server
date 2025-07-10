523c53e291d768801839a7a2bb0747678bc5954c
tlsalsco41122 <tlsalsco41122@gmail.com>
2025-07-10 13:46:12 +0900

    chore :: yml

--- src/main/java/com/olympus/uga/domain/calendar/domain/Schedule.java
+++ src/main/java/com/olympus/uga/domain/calendar/domain/Schedule.java
@@ -14,7 +14,6 @@ import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 import java.time.LocalDate;
-import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
 
