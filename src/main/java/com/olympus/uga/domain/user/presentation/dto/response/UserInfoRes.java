182536a5b51c52eb9af4f628bae3cf3d6d5bdfc4
tlsalsco41122 <tlsalsco41122@gmail.com>
2025-07-17 11:57:50 +0900

    feat :: add userId in userInfo

--- src/main/java/com/olympus/uga/domain/user/presentation/dto/response/UserInfoRes.java
+++ src/main/java/com/olympus/uga/domain/user/presentation/dto/response/UserInfoRes.java
@@ -4,10 +4,11 @@ import com.olympus.uga.domain.user.domain.User;
 import lombok.Builder;
 
 @Builder
-public record UserInfoRes(String username, String profileImage, boolean isLeader) {
+public record UserInfoRes(Long id, String username, String profileImage, boolean isLeader) {
     // 리더 여부를 직접 지정하는 팩토리 메서드
     public static UserInfoRes from(User user, boolean isLeader) {
         return UserInfoRes.builder()
+                .id(user.getId())
                 .username(user.getUsername())
                 .profileImage(user.getProfileImage())
                 .isLeader(isLeader)
@@ -17,6 +18,7 @@ public record UserInfoRes(String username, String profileImage, boolean isLeader
     // 기본값으로 리더가 아닌 것으로 설정하는 팩토리 메서드
     public static UserInfoRes from(User user) {
         return UserInfoRes.builder()
+                .id(user.getId())
                 .username(user.getUsername())
                 .profileImage(user.getProfileImage())
                 .isLeader(false)
