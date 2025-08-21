a63978d1fcc995c3c9745b6a133551f7f9433efa
tlsalsco41122 <tlsalsco41122@gmail.com>
2025-08-21 19:40:33 +0900

    fix :: family image

--- src/main/java/com/olympus/uga/domain/family/presentation/FamilyController.java
+++ src/main/java/com/olympus/uga/domain/family/presentation/FamilyController.java
@@ -8,9 +8,7 @@ import com.olympus.uga.global.common.Response;
 import com.olympus.uga.global.common.ResponseData;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,21 +17,8 @@ public class FamilyController {
     private final FamilyService familyService;
 
     @PostMapping(value = "/create")
-    @Operation(
-            summary = "가족 생성",
-            description = """
-                가족을 생성합니다.<br><br>
-                <b>Swagger에서 에러가 발생할 경우</b> 아래와 같이 <code>curl</code> 명령어를 사용해 터미널에서 테스트해주세요:
-                <pre>
-                curl -v -X POST "http://3.39.96.216/family/create" \\
-                  -H "Authorization: Bearer {액세스 토큰}" \\
-                  -F "familyProfile=@{이미지 파일 경로}" \\
-                  -F 'req={\"familyName\":\"가족이름\"};type=application/json'
-                </pre>
-                <br>
-                ⚠️ Swagger에서는 multipart/form-data + JSON 조합이 정상 동작하지 않을 수 있습니다.""")
-    public ResponseData<String> createFamily(
-            @RequestPart(name = "req") FamilyCreateReq req) {
+    @Operation(summary = "가족 생성")
+    public ResponseData<String> createFamily(@RequestBody FamilyCreateReq req) {
         return familyService.createFamily(req);
     }
 
