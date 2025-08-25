62f1f746b8e7ebd81e4a44fa0d42d7bc01ec6a9e
tlsalsco41122 <tlsalsco41122@gmail.com>
2025-08-25 15:32:27 +0900

    feat :: post create delete

--- src/main/java/com/olympus/uga/domain/album/service/AlbumService.java
+++ src/main/java/com/olympus/uga/domain/album/service/AlbumService.java
@@ -2,6 +2,8 @@ package com.olympus.uga.domain.album.service;
 
 import com.olympus.uga.domain.album.domain.Post;
 import com.olympus.uga.domain.album.domain.repo.PostJpaRepo;
+import com.olympus.uga.domain.album.error.PostErrorCode;
+import com.olympus.uga.domain.album.presentation.dto.request.PostReq;
 import com.olympus.uga.domain.album.presentation.dto.response.PostListRes;
 import com.olympus.uga.domain.family.domain.Family;
 import com.olympus.uga.domain.family.domain.repo.FamilyJpaRepo;
@@ -12,6 +14,7 @@ import com.olympus.uga.global.exception.CustomException;
 import com.olympus.uga.global.security.auth.UserSessionHolder;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -31,6 +34,28 @@ public class AlbumService {
         return postList.stream()
                 .map(PostListRes::from)
                 .toList();
+    }
+
+    @Transactional
+    public Response createPost(PostReq req) {
+        User user = userSessionHolder.getUser();
+
+        postJpaRepo.save(PostReq.fromPostReq(user, req));
+
+        return Response.created("게시글이 생성되었습니다.");
+    }
+
+    @Transactional
+    public Response deletePost(Long postId) {
+        User user = userSessionHolder.getUser();
+        String userFamilyCode = getUserFamilyCode(user.getId());
+
+        Post post = postJpaRepo.findByIdAndFamilyCode(postId, userFamilyCode)
+                        .orElseThrow(() -> new CustomException(PostErrorCode.POST_NOT_FOUND));
+
+        postJpaRepo.delete(post);
+
+        return Response.ok("게시글이 삭제되었습니다.");
     }
 
     private String getUserFamilyCode(Long userId) {
