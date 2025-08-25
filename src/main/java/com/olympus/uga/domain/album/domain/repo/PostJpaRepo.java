0d0b5eef0289c6b369e0073fb6b5308517268ec3
tlsalsco41122 <tlsalsco41122@gmail.com>
2025-08-25 15:32:27 +0900

    feat :: post create delete

--- src/main/java/com/olympus/uga/domain/album/domain/repo/PostJpaRepo.java
+++ src/main/java/com/olympus/uga/domain/album/domain/repo/PostJpaRepo.java
@@ -4,7 +4,9 @@ import com.olympus.uga.domain.album.domain.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface PostJpaRepo extends JpaRepository<Post, Long> {
     List<Post> findByFamilyCodeOrderByCreatedAtDesc(String familyCode);
+    Optional<Post> findByIdAndFamilyCode(Long id, String familyCode);
 }
