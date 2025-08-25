594808c0b34a0bf0916a9366572b88f907ca7cb2
tlsalsco41122 <tlsalsco41122@gmail.com>
2025-08-25 15:14:18 +0900

    feat :: post res dto

--- src/main/java/com/olympus/uga/domain/album/presentation/dto/response/PostRes.java
+++ src/main/java/com/olympus/uga/domain/album/presentation/dto/response/PostRes.java
@@ -1,4 +1,20 @@
 package com.olympus.uga.domain.album.presentation.dto.response;
 
-public record PostRes() {
+import com.olympus.uga.domain.album.domain.Post;
+import java.time.LocalDate;
+
+public record PostRes(String profileImage,
+                      String writerName,
+                      String content,
+                      String imageUrl,
+                      LocalDate createdAt) {
+    public static PostRes from(Post post) {
+        return new PostRes(
+                post.getWriter().getProfileImage(),
+                post.getWriter().getUsername(),
+                post.getContent(),
+                post.getImageUrl(),
+                post.getCreatedAt()
+        );
+    }
 }
