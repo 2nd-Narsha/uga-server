0d8693b425925495b30be1bb554c1d9b1a109dbd
tlsalsco41122 <tlsalsco41122@gmail.com>
2025-08-25 17:00:10 +0900

    feat :: comment setting

--- src/main/java/com/olympus/uga/domain/album/presentation/dto/response/PostListRes.java
+++ src/main/java/com/olympus/uga/domain/album/presentation/dto/response/PostListRes.java
@@ -8,15 +8,17 @@ public record PostListRes(Long postId,
                           String writerName,
                           String content,
                           String imageUrl,
-                          LocalDate createdAt) {
-    public static PostListRes from(Post post) {
+                          LocalDate createdAt,
+                          Long commentCount) {
+    public static PostListRes from(Post post, Long commentCount) {
         return new PostListRes(
                 post.getPostId(),
                 post.getWriter().getProfileImage(),
                 post.getWriter().getUsername(),
                 post.getContent(),
                 post.getImageUrl(),
-                post.getCreatedAt()
+                post.getCreatedAt(),
+                commentCount
         );
     }
 }
