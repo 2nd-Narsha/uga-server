672c5c7da40436e4e4f53475203ba53963eede12
tlsalsco41122 <tlsalsco41122@gmail.com>
2025-08-26 21:47:34 +0900

    feat :: post crud, comment, gallery

--- src/main/java/com/olympus/uga/domain/album/presentation/dto/response/GalleryRes.java
+++ src/main/java/com/olympus/uga/domain/album/presentation/dto/response/GalleryRes.java
@@ -1,7 +1,15 @@
 package com.olympus.uga.domain.album.presentation.dto.response;
 
+import com.olympus.uga.domain.album.domain.PostImage;
+
 import java.time.LocalDate;
 import java.util.List;
 
-public record GalleryRes(LocalDate date, List<String> imageUrls) {
+public record GalleryRes(LocalDate date, List<GalleryImageRes> images) {
+    public static GalleryRes from(LocalDate date, List<PostImage> images) {
+        List<GalleryImageRes> galleryImages = images.stream()
+                .map(GalleryImageRes::from)
+                .toList();
+        return new GalleryRes(date, galleryImages);
+    }
 }
