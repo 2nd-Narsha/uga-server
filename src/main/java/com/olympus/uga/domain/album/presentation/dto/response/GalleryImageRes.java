5e013520adcb44a7084b59a83824c27b9eb57cd7
tlsalsco41122 <tlsalsco41122@gmail.com>
2025-08-26 21:59:28 +0900

    fix :: GalleryImageRes

--- src/main/java/com/olympus/uga/domain/album/presentation/dto/response/GalleryImageRes.java
+++ src/main/java/com/olympus/uga/domain/album/presentation/dto/response/GalleryImageRes.java
@@ -2,13 +2,11 @@ package com.olympus.uga.domain.album.presentation.dto.response;
 
 import com.olympus.uga.domain.album.domain.PostImage;
 
-public record GalleryImageRes(Long postId, String imageUrl, String writerName, String profileImage) {
+public record GalleryImageRes(Long postId, String imageUrl) {
     public static GalleryImageRes from(PostImage postImage) {
         return new GalleryImageRes(
                 postImage.getPost().getPostId(),
-                postImage.getImageUrl(),
-                postImage.getPost().getWriter().getUsername(),
-                postImage.getPost().getWriter().getProfileImage()
+                postImage.getImageUrl()
         );
     }
 }
