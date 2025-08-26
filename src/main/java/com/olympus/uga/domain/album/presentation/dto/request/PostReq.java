c8aad2f5991ea0f1bba25093db6bdd282deecfd0
tlsalsco41122 <tlsalsco41122@gmail.com>
2025-08-26 21:56:44 +0900

    fix :: postReq

--- src/main/java/com/olympus/uga/domain/album/presentation/dto/request/PostReq.java
+++ src/main/java/com/olympus/uga/domain/album/presentation/dto/request/PostReq.java
@@ -5,8 +5,8 @@ import com.olympus.uga.domain.album.domain.PostImage;
 import com.olympus.uga.domain.user.domain.User;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.IntStream;
 
 public record PostReq(String content, List<String> imageUrls) {
     public static Post fromPostReq(User writer, PostReq req) {
@@ -18,13 +18,15 @@ public record PostReq(String content, List<String> imageUrls) {
 
         // 이미지 처리
         if (req.imageUrls() != null && !req.imageUrls().isEmpty()) {
-            List<PostImage> images = IntStream.range(0, req.imageUrls().size())
-                    .mapToObj(i -> PostImage.builder()
-                            .post(post)
-                            .imageUrl(req.imageUrls().get(i))
-                            .imageOrder(i + 1)
-                            .build())
-                    .toList();
+            List<PostImage> images = new ArrayList<>();
+            for (int i = 0; i < req.imageUrls().size(); i++) {
+                PostImage image = PostImage.builder()
+                        .post(post)
+                        .imageUrl(req.imageUrls().get(i))
+                        .imageOrder(i + 1)
+                        .build();
+                images.add(image);
+            }
             post.getImages().addAll(images);
         }
 
