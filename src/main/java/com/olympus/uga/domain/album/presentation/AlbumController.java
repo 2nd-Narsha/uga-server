12a47e41b83f3b3a57ac6f538a8ce7c2deb6e4c0
tlsalsco41122 <tlsalsco41122@gmail.com>
2025-08-27 09:49:12 +0900

    chore :: swagger operation

--- src/main/java/com/olympus/uga/domain/album/presentation/AlbumController.java
+++ src/main/java/com/olympus/uga/domain/album/presentation/AlbumController.java
@@ -8,6 +8,7 @@ import com.olympus.uga.domain.album.presentation.dto.response.PostListRes;
 import com.olympus.uga.domain.album.presentation.dto.response.PostRes;
 import com.olympus.uga.domain.album.service.AlbumService;
 import com.olympus.uga.global.common.Response;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -28,43 +29,51 @@ public class AlbumController {
 
     // 게시글 API
     @GetMapping("/posts")
+    @Operation(summary = "게시글 전체 조회")
     public List<PostListRes> getPosts() {
         return albumService.getPosts();
     }
 
     @GetMapping("/post/{postId}")
+    @Operation(summary = "게시글 상세 조회")
     public PostRes getPost(@PathVariable("postId") Long postId) {
         return albumService.getPost(postId);
     }
 
     @PostMapping("/post/create")
+    @Operation(summary = "게시글 생성")
     public Response createPost(@RequestBody PostReq req) {
         return albumService.createPost(req);
     }
 
     @PatchMapping("/post/update/{postId}")
+    @Operation(summary = "게시글 수정")
     public Response updatePost(@PathVariable("postId") Long postId, @RequestBody PostUpdateReq req) {
         return albumService.updatePost(postId, req);
     }
 
     @DeleteMapping("/post/delete/{postId}")
+    @Operation(summary = "게시글 삭제")
     public Response deletePost(@PathVariable("postId") Long postId) {
         return albumService.deletePost(postId);
     }
 
     // 댓글 API
     @PostMapping("/comment/{postId}")
+    @Operation(summary = "댓글 작성")
     public Response createComment(@PathVariable("postId") Long postId, @RequestBody CommentReq req) {
         return albumService.createComment(postId, req);
     }
 
     @DeleteMapping("/comment/delete/{commentId}")
+    @Operation(summary = "댓글 삭제")
     public Response deleteComment(@PathVariable("commentId") Long commentId) {
         return albumService.deleteComment(commentId);
     }
 
     // 갤러리 API
     @GetMapping("/gallery")
+    @Operation(summary = "이미지 모아보기")
     public List<GalleryRes> getGallery() {
         return albumService.getGallery();
     }
