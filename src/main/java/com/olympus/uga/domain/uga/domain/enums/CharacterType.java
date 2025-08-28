b7149a323f8874e4c6d2d6c36bfcfcb65d4f308c
tlsalsco41122 <tlsalsco41122@gmail.com>
2025-08-28 19:41:37 +0900

    feat :: uga deco enums

---
+++ src/main/java/com/olympus/uga/domain/uga/domain/enums/CharacterType.java
@@ -0,0 +1,6 @@
+package com.olympus.uga.domain.uga.domain.enums;
+
+public enum CharacterType {
+    UGA, // 기본
+    SOBORO, CHUNBOGI, BERINGKEON, BERRY, CHARLES, BONGGU, NICHOLAS, MILK, HODU, NAELLEUM, OGONGI, PENGDUGI, ACHIMI // 각 200포인트
+}
