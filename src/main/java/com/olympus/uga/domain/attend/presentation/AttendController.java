66f17216ef937b5892f77a5bc3f601e9d43af944
tlsalsco41122 <tlsalsco41122@gmail.com>
2025-08-28 17:37:17 +0900

    fix

--- src/main/java/com/olympus/uga/domain/attend/presentation/AttendController.java
+++ src/main/java/com/olympus/uga/domain/attend/presentation/AttendController.java
@@ -3,6 +3,7 @@ package com.olympus.uga.domain.attend.presentation;
 import com.olympus.uga.domain.attend.presentation.dto.response.AttendStatusRes;
 import com.olympus.uga.domain.attend.service.AttendService;
 import com.olympus.uga.global.common.Response;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,11 +17,13 @@ public class AttendController {
     private final AttendService attendService;
 
     @GetMapping("/status")
+    @Operation(summary = "출석 상태 조회")
     public AttendStatusRes getAttendStatus() {
         return attendService.getAttendStatus();
     }
 
     @PostMapping("/check")
+    @Operation(summary = "출석 체크")
     public Response checkAttend() {
         return attendService.checkAttend();
     }
