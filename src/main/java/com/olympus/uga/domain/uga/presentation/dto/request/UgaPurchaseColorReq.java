171d7016edd92b9ab1620d8f9f9537ea2c537219
tlsalsco41122 <tlsalsco41122@gmail.com>
2025-09-08 12:34:53 +0900

    fix :: uga deco

--- src/main/java/com/olympus/uga/domain/uga/presentation/dto/request/UgaPurchaseColorReq.java
+++
@@ -1,6 +0,0 @@
-package com.olympus.uga.domain.uga.presentation.dto.request;
-
-import com.olympus.uga.domain.uga.domain.enums.ColorType;
-
-public record UgaPurchaseColorReq(ColorType colorType) {
-}
