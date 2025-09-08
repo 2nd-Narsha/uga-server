a32db2586e29dae228019b3f0f9e6fbc0b60851d
tlsalsco41122 <tlsalsco41122@gmail.com>
2025-09-08 12:05:49 +0900

    chore :: swagger test

--- src/main/java/com/olympus/uga/global/config/SwaggerConfig.java
+++ src/main/java/com/olympus/uga/global/config/SwaggerConfig.java
@@ -17,7 +17,7 @@ public class SwaggerConfig {
                 .info(
                         new Info().title("Uga")
                                 .description("Uga API")
-                                .version("1.0.5")
+                                .version("1.0.6")
                 )
                 .addServersItem(new Server().url("https://uga.r-e.kr"))
                 .addServersItem(new Server().url("http://localhost:8080"))
