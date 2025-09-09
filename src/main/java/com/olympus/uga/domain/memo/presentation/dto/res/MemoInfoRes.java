4b053f6be43db5fff80ff29e936c3239551195e1
Ljh081021 <loveljh081021@dgsw.hs.kr>
2025-09-09 20:33:56 +0900

    fix :: return imageUrl on memo too

--- src/main/java/com/olympus/uga/domain/memo/presentation/dto/res/MemoInfoRes.java
+++ src/main/java/com/olympus/uga/domain/memo/presentation/dto/res/MemoInfoRes.java
@@ -4,11 +4,12 @@ import com.olympus.uga.domain.user.domain.User;
 import lombok.Builder;
 
 @Builder
-public record MemoInfoRes(Long id, String profileImage, String username, Double contribution, String memo, String location) {
+public record MemoInfoRes(Long id, String profileImage, String imageUrl, String username, Double contribution, String memo, String location) {
     public static MemoInfoRes from(Long id, User user, Double contribution, String memo, String location) {
         return MemoInfoRes.builder()
                 .id(id)
                 .profileImage(user.getProfileImage())
+                .imageUrl(user.getCharacter().getImageUrl())
                 .username(user.getUsername())
                 .contribution(contribution)
                 .memo(memo)
