5eeeb769e77c07cb78dbe5b2c2e7a5980a957d77
tlsalsco41122 <tlsalsco41122@gmail.com>
2025-09-09 19:54:09 +0900

    fix :: family delete

--- src/main/java/com/olympus/uga/domain/memo/domain/repo/MemoJpaRepo.java
+++ src/main/java/com/olympus/uga/domain/memo/domain/repo/MemoJpaRepo.java
@@ -3,11 +3,18 @@ package com.olympus.uga.domain.memo.domain.repo;
 import com.olympus.uga.domain.memo.domain.Memo;
 import com.olympus.uga.domain.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 @Repository
 public interface MemoJpaRepo extends JpaRepository<Memo, Long> {
     Optional<Memo> findByWriter(User user);
+
+    @Query("DELETE FROM Memo m WHERE m.family.familyCode = :familyCode")
+    @Modifying
+    void deleteByFamilyCode(@Param("familyCode") String familyCode);
 }
