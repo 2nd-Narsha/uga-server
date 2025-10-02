5712806b9efc7a84a16ea392c75b82b50cbe516c
Ljh081021 <loveljh081021@dgsw.hs.kr>
2025-10-02 09:04:45 +0900

    feat :: apple login

---
+++ src/main/java/com/olympus/uga/domain/oauth/presentation/dto/response/AppleTokenResponseDto.java
@@ -0,0 +1,22 @@
+package com.olympus.uga.domain.oauth.presentation.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+@Data
+public class AppleTokenResponseDto {
+    @JsonProperty("access_token")
+    private String accessToken;
+
+    @JsonProperty("token_type")
+    private String tokenType;
+
+    @JsonProperty("expires_in")
+    private String expiresIn;
+
+    @JsonProperty("refresh_token")
+    private String refreshToken;
+
+    @JsonProperty("id_token")
+    private String idToken;
+}
