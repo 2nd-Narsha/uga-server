df2b3d38437bfebdf1baed8876573b0d5e6980e5
Ljh081021 <loveljh081021@dgsw.hs.kr>
2025-10-02 09:04:45 +0900

    feat :: apple login

--- src/main/java/com/olympus/uga/domain/oauth/presentation/OAuthController.java
+++ src/main/java/com/olympus/uga/domain/oauth/presentation/OAuthController.java
@@ -1,10 +1,14 @@
 package com.olympus.uga.domain.oauth.presentation;
 
 import com.olympus.uga.domain.auth.presentation.dto.response.SignInRes;
+import com.olympus.uga.domain.oauth.service.AppleOAuthService;
 import com.olympus.uga.domain.oauth.usecase.OAuthUseCase;
+import com.olympus.uga.domain.user.domain.User;
 import com.olympus.uga.global.common.ResponseData;
 import io.swagger.v3.oas.annotations.Operation;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -26,5 +30,12 @@ public class OAuthController {
     @Operation(summary = "구글 oauth")
     public ResponseData<SignInRes> googleLogin(@RequestParam("accessToken") String accessToken) {
         return oAuthUseCase.loginWithGoogleToken(accessToken);
+    }
+
+    @PostMapping("/apple/login")
+    @Operation(summary = "애플 oauth")
+    public ResponseEntity<ResponseData<SignInRes>> appleRedirect(@RequestParam("code") String code) {
+        ResponseData<SignInRes> response = oAuthUseCase.appleLogin(code);
+        return ResponseEntity.ok(response);
     }
 }
